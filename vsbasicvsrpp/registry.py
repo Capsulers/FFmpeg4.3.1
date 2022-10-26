476a5d344165b0c8d7a8c831695964bb83226a97
Capsulers <113909207+Capsulers@users.noreply.github.com>
2022-10-27 02:55:30 +0800

    Delete vsbasicvsrpp directory

--- vsbasicvsrpp/registry.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from mmcv.utils import Registry
-
-MODELS = Registry('model')
-BACKBONES = Registry('backbone')
-COMPONENTS = Registry('component')
-LOSSES = Registry('loss')
